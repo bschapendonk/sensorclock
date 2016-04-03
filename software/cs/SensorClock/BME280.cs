d58460c0b7cd17bc23e0c405b56b5660121e3610
Bart Schapendonk <bart@schapendonk.com>
2016-04-03 16:52:42 +0200

    update

--- software/cs/SensorClock/BME280.cs
+++ software/cs/SensorClock/BME280.cs
@@ -145,8 +145,8 @@ namespace SensorClock
         double BME280_compensate_T_double(int adc_T)
         {
             double var1, var2, T;
-            var1 = (((double)adc_T) / 16384.0 - ((double)dig_T1) / 1024.0) * ((double)dig_T2);
-            var2 = ((((double)adc_T) / 131072.0 - ((double)dig_T1) / 8192.0) * (((double)adc_T) / 131072.0 - ((double)dig_T1) / 8192.0)) * ((double)dig_T3);
+            var1 = ((adc_T) / 16384.0 - (dig_T1) / 1024.0) * (dig_T2);
+            var2 = (((adc_T) / 131072.0 - (dig_T1) / 8192.0) * ((adc_T) / 131072.0 - (dig_T1) / 8192.0)) * (dig_T3);
             t_fine = (int)(var1 + var2);
             T = (var1 + var2) / 5120.0;
             return T;
@@ -156,21 +156,21 @@ namespace SensorClock
         double BME280_compensate_P_double(int adc_P)
         {
             double var1, var2, p;
-            var1 = ((double)t_fine / 2.0) - 64000.0;
-            var2 = var1 * var1 * ((double)dig_P6) / 32768.0;
-            var2 = var2 + var1 * ((double)dig_P5) * 2.0;
-            var2 = (var2 / 4.0) + (((double)dig_P4) * 65536.0);
-            var1 = (((double)dig_P3) * var1 * var1 / 524288.0 + ((double)dig_P2) * var1) / 524288.0;
-            var1 = (1.0 + var1 / 32768.0) * ((double)dig_P1);
+            var1 = (t_fine / 2.0) - 64000.0;
+            var2 = var1 * var1 * (dig_P6) / 32768.0;
+            var2 = var2 + var1 * (dig_P5) * 2.0;
+            var2 = (var2 / 4.0) + ((dig_P4) * 65536.0);
+            var1 = ((dig_P3) * var1 * var1 / 524288.0 + (dig_P2) * var1) / 524288.0;
+            var1 = (1.0 + var1 / 32768.0) * (dig_P1);
             if (var1 == 0.0)
             {
                 return 0; // avoid exception caused by division by zero
             }
-            p = 1048576.0 - (double)adc_P;
+            p = 1048576.0 - adc_P;
             p = (p - (var2 / 4096.0)) * 6250.0 / var1;
-            var1 = ((double)dig_P9) * p * p / 2147483648.0;
-            var2 = p * ((double)dig_P8) / 32768.0;
-            p = p + (var1 + var2 + ((double)dig_P7)) / 16.0;
+            var1 = (dig_P9) * p * p / 2147483648.0;
+            var2 = p * (dig_P8) / 32768.0;
+            p = p + (var1 + var2 + (dig_P7)) / 16.0;
             return p;
         }
 
@@ -178,9 +178,9 @@ namespace SensorClock
         double BME280_compensate_H_double(int adc_H)
         {
             double var_H;
-            var_H = (((double)t_fine) - 76800.0);
-            var_H = (adc_H - (((double)dig_H4) * 64.0 + ((double)dig_H5) / 16384.0 * var_H)) * (((double)dig_H2) / 65536.0 * (1.0 + ((double)dig_H6) / 67108864.0 * var_H * (1.0 + ((double)dig_H3) / 67108864.0 * var_H)));
-            var_H = var_H * (1.0 - ((double)dig_H1) * var_H / 524288.0);
+            var_H = ((t_fine) - 76800.0);
+            var_H = (adc_H - ((dig_H4) * 64.0 + (dig_H5) / 16384.0 * var_H)) * ((dig_H2) / 65536.0 * (1.0 + (dig_H6) / 67108864.0 * var_H * (1.0 + (dig_H3) / 67108864.0 * var_H)));
+            var_H = var_H * (1.0 - (dig_H1) * var_H / 524288.0);
             if (var_H > 100.0)
                 var_H = 100.0;
             else if (var_H < 0.0)
