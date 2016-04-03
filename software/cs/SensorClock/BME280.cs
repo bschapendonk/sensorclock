fd07a21eca5dc5094a3a3e20ae49cc4c431638a3
Bart Schapendonk <bart@schapendonk.com>
2016-04-03 16:40:39 +0200

    update

--- software/cs/SensorClock/BME280.cs
+++ software/cs/SensorClock/BME280.cs
@@ -36,7 +36,7 @@ namespace SensorClock
         short dig_T2, dig_T3, dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9, dig_H2, dig_H4, dig_H5;
         byte dig_H1, dig_H3;
         sbyte dig_H6;
-        long t_fine;
+        int t_fine;
         #endregion
 
         I2cDevice _bme280;
@@ -114,12 +114,12 @@ namespace SensorClock
             return (buffer[offset] << 12) + (buffer[offset + 1] << 4) + (buffer[offset + 2] >> 4);
         }
 
-        short ReadSignedShort(byte[] buffer, int offset = 0)
+        int ReadSignedShort(byte[] buffer, int offset = 0)
         {
             if (offset + 1 >= buffer.Length)
                 throw new ArgumentOutOfRangeException(nameof(offset));
 
-            return (short)((buffer[offset] << 8) + buffer[offset + 1]);
+            return (buffer[offset] << 8) + buffer[offset + 1];
         }
 
         short ReadSignedShortLittleEndian(byte[] buffer, int offset = 0)
@@ -139,49 +139,55 @@ namespace SensorClock
         }
 
         #region Compensation formulas straight from the datasheet
-        // Returns temperature in DegC, resolution is 0.01 DegC. Output value of “5123” equals 51.23 DegC.
+
+        // Returns temperature in DegC, double precision. Output value of “51.23” equals 51.23 DegC.
         // t_fine carries fine temperature as global value
         double BME280_compensate_T_double(int adc_T)
         {
-            var var1 = (adc_T / 16384.0 - dig_T1 / 1024.0) * dig_T2;
-            var var2 = ((adc_T / 131072.0 - dig_T1 / 8192.0) * (adc_T / 131072.0 - dig_T1 / 8192.0)) * dig_T3;
+            double var1, var2, T;
+            var1 = (((double)adc_T) / 16384.0 - ((double)dig_T1) / 1024.0) * ((double)dig_T2);
+            var2 = ((((double)adc_T) / 131072.0 - ((double)dig_T1) / 8192.0) * (((double)adc_T) / 131072.0 - ((double)dig_T1) / 8192.0)) * ((double)dig_T3);
             t_fine = (int)(var1 + var2);
-            return (var1 + var2) / 5120.0;
+            T = (var1 + var2) / 5120.0;
+            return T;
         }
 
         // Returns pressure in Pa as double. Output value of “96386.2” equals 96386.2 Pa = 963.862 hPa
         double BME280_compensate_P_double(int adc_P)
         {
-            var var1 = (t_fine / 2.0) - 64000.0;
-            var var2 = var1 * var1 * dig_P6 / 32768.0;
-            var2 = var2 + var1 * dig_P5 * 2.0;
-            var2 = (var2 / 4.0) + (dig_P4 * 65536.0);
-            var1 = (dig_P3 * var1 * var1 / 524288.0 + dig_P2 * var1) / 524288.0;
-            var1 = (1.0 + var1 / 32768.0) * dig_P1;
+            double var1, var2, p;
+            var1 = ((double)t_fine / 2.0) - 64000.0;
+            var2 = var1 * var1 * ((double)dig_P6) / 32768.0;
+            var2 = var2 + var1 * ((double)dig_P5) * 2.0;
+            var2 = (var2 / 4.0) + (((double)dig_P4) * 65536.0);
+            var1 = (((double)dig_P3) * var1 * var1 / 524288.0 + ((double)dig_P2) * var1) / 524288.0;
+            var1 = (1.0 + var1 / 32768.0) * ((double)dig_P1);
             if (var1 == 0.0)
             {
                 return 0; // avoid exception caused by division by zero
             }
-            var p = 1048576.0 - adc_P;
+            p = 1048576.0 - (double)adc_P;
             p = (p - (var2 / 4096.0)) * 6250.0 / var1;
-            var1 = dig_P9 * p * p / 2147483648.0;
-            var2 = p * dig_P8 / 32768.0;
-            p = p + (var1 + var2 + dig_P7) / 16.0;
+            var1 = ((double)dig_P9) * p * p / 2147483648.0;
+            var2 = p * ((double)dig_P8) / 32768.0;
+            p = p + (var1 + var2 + ((double)dig_P7)) / 16.0;
             return p;
         }
 
         // Returns humidity in %rH as as double. Output value of “46.332” represents 46.332 %rH
         double BME280_compensate_H_double(int adc_H)
         {
-            var var_H = ((t_fine) - 76800.0);
-            var_H = (adc_H - ((dig_H4) * 64.0 + dig_H5 / 16384.0 * var_H)) * ((dig_H2) / 65536.0 * (1.0 + dig_H6 / 67108864.0 * var_H * (1.0 + dig_H3 / 67108864.0 * var_H)));
-            var_H = var_H * (1.0 - dig_H1 * var_H / 524288.0);
+            double var_H;
+            var_H = (((double)t_fine) - 76800.0);
+            var_H = (adc_H - (((double)dig_H4) * 64.0 + ((double)dig_H5) / 16384.0 * var_H)) * (((double)dig_H2) / 65536.0 * (1.0 + ((double)dig_H6) / 67108864.0 * var_H * (1.0 + ((double)dig_H3) / 67108864.0 * var_H)));
+            var_H = var_H * (1.0 - ((double)dig_H1) * var_H / 524288.0);
             if (var_H > 100.0)
                 var_H = 100.0;
             else if (var_H < 0.0)
                 var_H = 0.0;
             return var_H;
         }
+
         #endregion
 
         public void Dispose()
