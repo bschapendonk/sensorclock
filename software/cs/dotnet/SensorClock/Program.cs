81e7ee25091bfd388998e8943e7bb4bb38ad237a
Bart Schapendonk <bart@schapendonk.com>
2022-11-16 18:58:50 +0100

    Raspberry Pi 4B get! Hello World ARM64!

--- software/cs/dotnet/SensorClock/Program.cs
+++ software/cs/dotnet/SensorClock/Program.cs
@@ -13,3 +13,14 @@ var host = Host.CreateDefaultBuilder(args)
 await host.RunAsync();
 
 // https://blog.maartenballiauw.be/post/2021/05/25/running-a-net-application-as-a-service-on-linux-with-systemd.html
+/* sudo nano -w /etc/systemd/system/sensorclock.service
+ * 
+ * sudo systemctl daemon-reload
+ * sudo systemctl enable sensorclock.service
+ * 
+ * sudo systemctl start sensorclock.service
+ * sudo systemctl stop sensorclock.service
+ * 
+ * sudo systemctl status sensorclock.service
+ * 
+ */
