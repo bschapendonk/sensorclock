2dae03a0e81817ce6e20558e8a2d21bc1fa5e033
Bart Schapendonk <bart@schapendonk.com>
2023-05-02 17:19:03 +0200

    update

--- software/python/circuitpython/code.py
+++ software/python/circuitpython/code.py
@@ -4,7 +4,7 @@ import adafruit_ntp
 import asyncio
 import board
 import busio
-import constants
+import PCA9622
 import digitalio
 import ipaddress
 import os
@@ -19,6 +19,8 @@ wifi_ssid = os.getenv('WIFI_SSID')
 wifi_password = os.getenv('WIFI_PASSWORD')
 ntp_server = os.getenv('NTP_SERVER')
 tz_name = os.getenv('TZ_NAME')
+
+current_second = bytearray(17)
 
 wifi.radio.connect(wifi_ssid, wifi_password)
 print("Connected to WiFi")
@@ -41,11 +43,20 @@ def init():
     # shdn_3v3.direction = digitalio.Direction.OUTPUT
     # shdn_3v3.value = True
 
+def display_init():
+    i2c = busio.I2C(scl=board.GP5, sda=board.GP4)
+    i2c.writeto(PCA9622.ADDR_ALLCALL, bytes([PCA9622.REGISTER_MODE1, PCA9622.MODE1_SUBADDR1 | PCA9622.MODE1_ALLCALL ]))
+    time.sleep_ms(10)
+    i2c.writeto(PCA9622.ADDR_ALLCALL, bytes([PCA9622.REGISTER_GRPPWM, PCA9622.PWM_DEFAULT]))
+    i2c.writeto(PCA9622.ADDR_ALLCALL, bytes([PCA9622.REGISTER_PWM0 | PCA9622.AUTO_INCREMENT, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
+    i2c.writeto(PCA9622.ADDR_ALLCALL, bytes([PCA9622.REGISTER_LEDOUT0 | PCA9622.AUTO_INCREMENT, 0xFF, 0xFF, 0xFF, 0xFF]))
+    i2c.writeto(PCA9622.ADDR_MINUTE, bytes([PCA9622.REGISTER_MODE1, PCA9622.MODE1_ALLCALL]))
+    i2c.writeto(PCA9622.ADDR_HOUR, bytes([PCA9622.REGISTER_MODE1, PCA9622.MODE1_ALLCALL]))
+
 async def rtc_update():
     while True:
         rtc.RTC().datetime = ntp.datetime
-
-        await asyncio.sleep(600)
+        await asyncio.sleep(300)
 
 async def display_update():
     while True:
@@ -54,9 +65,17 @@ async def display_update():
         # print("UTC: {}".format(utc_now_dt.ctime()))
         localtime = utc_now_dt + timezone(tz_name).utcoffset(utc_now_dt)
         print("{}: {}".format(tz_name, localtime.ctime()))
+
+        # if(localtime.propertymicrosecond > 500000)
+        #     current_second[11] = 0xFF
+
+        # i2c.writeto(PCA9622.ADDR_SECOND, bytes(PCA9622.DIGITS[localtime.second]))
+        # i2c.writeto(PCA9622.ADDR_MINUTE, bytes(PCA9622.DIGITS[localtime.minute]))
+        # i2c.writeto(PCA9622.ADDR_HOUR, bytes(PCA9622.DIGITS[localtime.hour]))
+
         # print(constants._DIGITS[localtime.second], sep = ", ")
 
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
         # await asyncio.sleep_ms(500)
 
 async def main():
